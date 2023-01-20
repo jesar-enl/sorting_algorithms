bbc890cbb893b5713a9a2fb4cbb138f9e914b6e0
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-01-20 11:50:05 +0300

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - to sort the list
+ *
+ * @array: the array to be sorted
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+	int swap;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
+				print_array(array, size);
+			}
+		}
+	}
+}
